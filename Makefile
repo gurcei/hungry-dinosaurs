a3120382ae7c49571fe02cb2edb4ec0d6bf96dcc
Gurce Isikyildiz <Gurce.Isikyildiz@resmed.com.au>
2023-04-13 16:43:52 +1000

    Got Ayca's converter tool working for .svg to vart conversion :)
    E.g. "UZBEKagain.svg" --> "uzbek.v"
    
    (which I then copy over to the '11.D81' image as 'test.v' via make)

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 all:
 	cp /c/Users/gurcei/AppData/Roaming/xemu-lgb/mega65/hdos/11.D81 .
 	c1541 -attach 11.D81 -read viet.v,s -read thai.v,s -read aus.v,s -read turk.v,s
+	c1541 -attach /c/Users/gurcei/AppData/Roaming/xemu-lgb/mega65/hdos/11.D81 -delete test.v -write uzbek.v "test.v,s"
---
+++ svgconv.py
@@ -0,0 +1,201 @@
+# Tool to convert Ayca's .svg exports into a format suitable for MEGA65 vector-art
+import xml.etree.ElementTree as ET
+import re
+import struct
+
+def read_xml():
+    return ET.parse('UZBEKagain.svg').getroot()
+
+LINE    = 0
+CIRCLE  = 1
+ELLIPSE = 2
+BOX     = 3
+POLY    = 4
+COLOUR = 63
+FILL_FLAG = 64
+
+scalex = 640/64000
+scaley = 320 / 32047
+
+map_svg_to_m65_clr = {
+    'none': 3,
+    'black': 0,
+    '#FF7777': 10,
+    '#BBBBBB': 15
+}
+
+map_clrname_to_m65clr = { }
+
+previous_colour = -1
+selected_colour = -1
+
+def extract_colours(xml):
+    clrs = xml[0][0].text.strip()
+    for line in clrs.splitlines():
+        line = line.strip()
+        print(line)
+        s = line.split(' ')
+        name=s[0]
+        m = re.match(r'.*:(.*)}', s[1])
+        colval=m.groups()[0]
+        map_clrname_to_m65clr[name] = map_svg_to_m65_clr[colval]
+
+    print(map_clrname_to_m65clr)
+
+    return map_clrname_to_m65clr
+
+def check_for_colour_change(clr):
+    global selected_colour
+    if clr != selected_colour:
+        selected_colour = clr
+        outfile.write(struct.pack('B', COLOUR))
+        outfile.write(struct.pack('B', clr))
+
+def parse_polygon(poly):
+    clr = map_clrname_to_m65clr['.'+poly['class']]
+    check_for_colour_change(clr)
+
+    ptxt = poly['points']
+    minx = 0
+    miny = 0
+    maxx = 0
+    maxy = 0
+    # scalex = 1
+    # scaley = 1
+    points = []
+    for coord in ptxt.strip().split(' '):
+        tx,ty = coord.split(',')
+        x = int(tx)
+        y = int(ty)
+    #     if x<minx:
+    #         minx = x
+    #     if x>maxx:
+    #         maxx = x
+    #     if y<miny:
+    #         miny=y
+    #     if y>maxy:
+    #         maxy=y
+        points.append([x * scalex, y * scaley])
+
+    # print(points)
+    # print('minx={}, maxx={}'.format(minx, maxx))
+    # print('miny={}, maxy={}'.format(miny, maxy))
+
+    final_points = points
+    # if maxx > 639:
+    #     scalex = 639 / maxx
+    # if maxy > 399:
+    #     scaley = 399 / maxy
+
+    # scale = scalex
+    # if scaley < scalex:
+    #     scale = scaley
+
+    # final_points = []
+    # for coord in points:
+    #     x = coord[0] * scale
+    #     y = coord[1] * scale
+    #     final_points.append([x,y])
+
+    # save out to vart file
+    outfile.write(struct.pack('B', POLY + FILL_FLAG))
+    outfile.write(struct.pack('B', len(final_points)))
+    for coord in final_points:
+        outfile.write(struct.pack('<H', int(coord[0])))
+        outfile.write(struct.pack('<H', int(coord[1])))
+
+def parse_path(path):
+    clr = map_clrname_to_m65clr['.'+path['class']]
+    check_for_colour_change(clr)
+
+    absx = 0
+    absy = 0
+
+    points = []
+
+    txt = path['d']
+    print('txt = {}'.format(txt))
+    while len(txt) > 0:
+        m = re.match(r'[Mmclz](.*?)[Mmclz].*', txt)
+        if m:
+            t = m.groups()[0]
+        else:
+            t = ""
+        cmd = txt[0]
+        print('clr = {} : cmd = {} : t = {}'.format(clr, cmd, t))
+        txt=txt[len(t)+1:]
+
+        if cmd == 'M':
+            tx,ty = t.split(' ')
+            x = int(tx)
+            y = int(ty)
+            absx = x
+            absy = y
+            points.append([absx * scalex, absy * scaley])
+
+        if cmd == 'z':
+            # save out to vart file
+            v = POLY + FILL_FLAG
+            if path['class'] == 'fil3':
+                v = POLY
+            outfile.write(struct.pack('B', v))
+            outfile.write(struct.pack('B', len(points)))
+            for coord in points:
+                print(coord)
+                outfile.write(struct.pack('<H', int(coord[0])))
+                outfile.write(struct.pack('<H', int(coord[1])))
+            points=[]
+
+        if cmd == 'm':
+            tx,ty = t.split(' ')
+            x = int(tx)
+            y = int(ty)
+            absx += x
+            absy += y
+            points.append([absx * scalex, absy * scaley])
+
+        if cmd == 'c':
+            cnt = 0
+            for coord in t.strip().split(' '):
+                tx,ty = coord.split(',')
+                if cnt == 2:
+                    absx += int(tx)
+                    absy += int(ty)
+                    points.append([absx * scalex, absy * scaley])
+                    cnt = 0
+                else:
+                    cnt += 1
+
+        if cmd == 'l':
+            cnt = 0
+            for ival in t.strip().split(' '):
+                if cnt % 2 == 0:
+                    absx += int(ival)
+                if cnt % 2 == 1:
+                    absy += int(ival)
+                    points.append([absx * scalex, absy * scaley])
+                cnt += 1
+
+xml = read_xml()
+
+colours = extract_colours(xml)
+
+outfile = open('uzbek.v', 'wb')
+outfile.write(b'VEC')
+
+# iterate over objects (polygons/paths)
+for item in xml[1]:
+    if 'polygon' in item.tag:
+        parse_polygon(item.attrib)
+    elif 'path' in item.tag:
+        parse_path(item.attrib)
+
+    # print(item.tag)
+
+outfile.close()
+
+# scan their x,y coord details to keep track of minx/y and maxx/y
+
+# store objects in a scaling that fits within 640x400 (or 640x320)
+
+# save out a "uzbek.v" file in vart-format
